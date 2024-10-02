ca80d816722b1ed06ef5fcb1c4f7f919b3e1f944
reyes-ale <147879016+reyes-ale@users.noreply.github.com>
2024-10-02 16:37:09 -0600

    Add files via upload

---
+++ Pre-proyecto-GUI/src/pre/proyecto/gui/PreProyectoGUI.java
@@ -0,0 +1,17 @@
+
+package pre.proyecto.gui;
+
+/**
+ *
+ * @author aleja
+ */
+public class PreProyectoGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
